c10e5845dbd22d635613a19d884a90dced98438a
spray91 <spray1@gmail.com>
2018-09-09 21:51:48 +0200

    creation of repot + gitbash test

---
+++ src/main/java/pl/spray/restdemo/transit/TransitApplication.java
@@ -0,0 +1,12 @@
+package pl.spray.restdemo.transit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/spray/restdemo/transit/TransitApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.spray.restdemo.transit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TransitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
