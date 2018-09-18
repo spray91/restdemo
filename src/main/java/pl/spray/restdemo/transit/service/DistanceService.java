a241c070fca9279dd8a970048450cae6bbdd7fde
spray91 <spray1@gmail.com>
2018-09-18 21:02:36 +0200

    Modification of Distance Service. Now it will return only distance distance.

--- src/main/java/pl/spray/restdemo/transit/service/DistanceService.java
+++ src/main/java/pl/spray/restdemo/transit/service/DistanceService.java
@@ -4,12 +4,9 @@ import java.math.BigDecimal;
 import java.util.Optional;
 
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -28,39 +25,24 @@ public class DistanceService {
 	@Autowired
 	TransitUtilities utilities;
 
-	@Value("${mapquestapi.key}")
-	private String key;
-
 	@Async
 	public void getDistance(Long id) throws InterruptedException, JSONException {
 
 		logger.info(String.format("Processing request with id %d", id));
 
-		BigDecimal distance;
 		Optional<TransitModel> transitmodel = dao.findById(id);
 
-		ResponseEntity<String> distanceResponse = utilities.sendDistanceRequest(transitmodel.get().getSource(),
+		BigDecimal distance = utilities.getDistance(transitmodel.get().getSource(),
 				transitmodel.get().getDestination());
 
-		JSONObject distanceJson = new JSONObject(distanceResponse.getBody());
-
-		try {
-			distance = BigDecimal.valueOf(distanceJson.getJSONArray("distance")
-					.getDouble(distanceJson.getJSONArray("distance").length() - 1));
-		} catch (JSONException ex) {
-
-			transitmodel.get().setErrorMssage(
-					distanceJson.getJSONObject("info").getJSONArray("messages").getString(0).toString());
-			dao.save(transitmodel.get());
-
+		if (distance.compareTo(BigDecimal.ZERO) < 0) {
+			transitmodel.get().setErrorMssage("WWe are not able to determine the distance between the locations");
 			logger.info(String.format("Request with id %d was finished with errors", id));
-			return;
+		} else {
+			distance = distance.multiply(new BigDecimal(1.609));
+			transitmodel.get().setDistance(distance);
+			logger.info(String.format("Request with id %d was finished without errors", id));
 		}
-
-		distance = distance.multiply(new BigDecimal(1.609));
-		transitmodel.get().setDistance(distance);
-
-		logger.info(String.format("Request with id %d was finished without errors", id));
 
 		dao.save(transitmodel.get());
 	}
--- src/main/java/pl/spray/restdemo/transit/utilities/TransitUtilities.java
+++ src/main/java/pl/spray/restdemo/transit/utilities/TransitUtilities.java
@@ -1,5 +1,7 @@
 package pl.spray.restdemo.transit.utilities;
 
+import java.math.BigDecimal;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,8 +21,9 @@ public class TransitUtilities {
 	@Value("${mapquestapi.key}")
 	private String key;
 
-	public ResponseEntity<String> sendDistanceRequest(String source, String destination) {
+	public BigDecimal getDistance(String source, String destination) throws JSONException {
 
+		BigDecimal distance;
 		RestTemplate restTemplate = new RestTemplate();
 
 		String url = String.format("http://www.mapquestapi.com/directions/v2/routematrix?key=%s", key);
@@ -30,14 +33,25 @@ public class TransitUtilities {
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		HttpEntity<String> entity = new HttpEntity<String>(requestJson, headers);
 
-		return restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+		ResponseEntity<String> distanceResponse = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+
+		JSONObject distanceJson = new JSONObject(distanceResponse.getBody());
+
+		try {
+			distance = BigDecimal.valueOf(distanceJson.getJSONArray("distance")
+					.getDouble(distanceJson.getJSONArray("distance").length() - 1));
+		} catch (JSONException ex) {
+
+			return new BigDecimal(-1);
+		}
+		return distance;
+
 	}
 
 	public JSONObject createErrorJSON(TransitModel transit) throws JSONException {
 
 		return new JSONObject().put("Transit source", transit.getSource())
-				.put("transit_destination", transit.getDestination())
-				.put("distance", 0)
+				.put("transit_destination", transit.getDestination()).put("distance", 0)
 				.put("error_message", transit.getErrorMssage());
 	}
 }
