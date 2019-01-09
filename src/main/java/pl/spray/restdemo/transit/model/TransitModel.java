a04eb55eb44dd11fce2082235ed68dc3cb1ff8c2
spray91 <spray1@gmail.com>
2019-01-09 14:43:38 +0100

    added lombok, deleted unused constructor

--- src/main/java/pl/spray/restdemo/transit/model/TransitModel.java
+++ src/main/java/pl/spray/restdemo/transit/model/TransitModel.java
@@ -1,5 +1,8 @@
 package pl.spray.restdemo.transit.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -12,7 +15,9 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
 @Entity
+@Data
 @Table(name = "transit")
+@NoArgsConstructor
 public class TransitModel {
 
 	@Id
@@ -33,76 +38,10 @@ public class TransitModel {
 
 	private LocalDate date;
 
-	private String errorMssage;
-
-	protected TransitModel() {
-	}
-
-	public TransitModel(String source, String destination, BigDecimal price, LocalDate date) {
-		this.source = source;
-		this.destination = destination;
-		this.price = price;
-		this.date = date;
-	}
+	private String errorMessage;
 
 	@Override
 	public String toString() {
 		return String.format("ID: %d", id);
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public BigDecimal getPrice() {
-		return price;
-	}
-
-	public void setPrice(BigDecimal price) {
-		this.price = price;
-	}
-
-	public BigDecimal getDistance() {
-		return distance;
-	}
-
-	public void setDistance(BigDecimal distance) {
-		this.distance = distance;
-	}
-
-	public String getSource() {
-		return source;
-	}
-
-	public void setSource(String source) {
-		this.source = source;
-	}
-
-	public String getDestination() {
-		return destination;
-	}
-
-	public void setDestination(String destination) {
-		this.destination = destination;
-	}
-
-	public LocalDate getDate() {
-		return date;
-	}
-
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
-
-	public String getErrorMssage() {
-		return errorMssage;
-	}
-
-	public void setErrorMssage(String errorMssage) {
-		this.errorMssage = errorMssage;
 	}
 }
